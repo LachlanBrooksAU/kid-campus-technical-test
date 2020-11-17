1a5606bf2e8cffdf8692a5819080009bd9c017ab
LachlanBrooks <lachlan.brooks@crestrock.com.au>
2020-11-18 10:53:57 +1100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <input placeholder='Enter Postcode or suburb' />
+        <button>Search</button>
+      </div>
     </div>
   );
 }
